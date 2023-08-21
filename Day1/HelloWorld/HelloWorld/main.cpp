65497ba244ec96cbf218480ffe49ee0231f85348
andrew delis <andrew.s.delis@gmail.com>
2023-08-21 12:17:28 -0600

    committing my hello world project to git

---
+++ Day1/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Andrew Delis on 8/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World! Here is so additional info\n";
+    std::cout << "2 + 2 = 4\n";
+    return 0;
+}
