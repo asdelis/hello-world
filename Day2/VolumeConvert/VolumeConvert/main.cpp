d3615c9296e58ab6ecc2958801b26f62823b5f98
andrew delis <andrew.s.delis@gmail.com>
2023-08-22 12:26:13 -0600

    added Josh's part

--- Day2/VolumeConvert/VolumeConvert/main.cpp
+++ Day2/VolumeConvert/VolumeConvert/main.cpp
@@ -40,5 +40,30 @@ int main(int argc, const char * argv[]) {
     std::cout << "Cubic Inches: " << cubicInches << std::endl;
     
     //output the conversions
+    
+    // Part 1 by Andrew Delis
+    
+      //create score variables for the 5 scores we are requesting.
+      float score1;
+      float score2;
+      float score3;
+      float score4;
+      float score5;
+      // prompt user by displaying a message to enter 5 assignment scores
+      std::cout << "Enter 5 Assignment Scores:\n";
+      std::cin >> score1;
+      std::cin >> score2;
+      std::cin >> score3;
+      std::cin >> score4;
+      std::cin >> score5;
+      // create an average variable that averages all scores
+      float average = (score1 + score2 + score3 + score4 + score5) / 5;
+      // display the average score to the user
+      std::cout << "Average Score: ";
+      std::cout << average;
+    
+    //Part 2 by Josh Barton
+    
+    
     return 0;
 }
