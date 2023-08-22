7a7c07580ea03b16392a250b06c341785cc0619e
andrew delis <andrew.s.delis@gmail.com>
2023-08-22 11:51:42 -0600

    Fixed my step 2 so that I didn't have the variable randomly at the end of the prompts. Also added a note.

--- Day2/RoadTripCalculator/RoadTripCalculator/main.cpp
+++ Day2/RoadTripCalculator/RoadTripCalculator/main.cpp
@@ -15,25 +15,28 @@ int main(int argc, const char * argv[]) {
     
     //Step 1: Making some variables
     
-    std::cout << "Please enter the distance of your trip in whole miles" << tripDistance << std::endl;
+    std::cout << "Please enter the distance of your trip in whole miles" << std::endl;
     std::cin >> tripDistance;
-    std::cout << "Please enter the efficiency of your car in miles per gallon" << milesPerGallon << std::endl;
+    std::cout << "Please enter the efficiency of your car in miles per gallon" << std::endl;
     std::cin >> milesPerGallon;
-    std::cout << "Please enter the price of gas in USD" << gasCost << std::endl;
+    std::cout << "Please enter the price of gas in USD" << std::endl;
     std::cin >> gasCost;
     
     //Step 2: Allowing inputs for the variables we made in Step 1
     
     float gallonsPerTrip = tripDistance/milesPerGallon;
     
-    //^ Step 3: creating a new variable
-    //  that is the number of gallons of gas needed per trip
+    //Step 3: creating a new variable
+    //that is the number of gallons of gas needed per trip
     
     float tripCost = gallonsPerTrip * gasCost;
     
-    //^ Step 4: creating a new variable
-    //  that is the total cost of of the trip
+    //Step 4: creating a new variable
+    //that is the total cost of of the trip
     
+    std::cout << "The total cost of your trip is " << "$" << tripCost << std::endl;
+    
+    //Step 5: Output the result of the program
     
     return 0;
 }
