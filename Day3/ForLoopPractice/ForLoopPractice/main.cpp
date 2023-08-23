41ff8b8350bc8db81bd18c9a18c924b52f0b9aa7
andrew delis <andrew.s.delis@gmail.com>
2023-08-23 13:08:25 -0600

    I forgot to do a couple steps in the practice problems. These updated include new code for that.

--- Day3/ForLoopPractice/ForLoopPractice/main.cpp
+++ Day3/ForLoopPractice/ForLoopPractice/main.cpp
@@ -9,44 +9,48 @@
 
 int main(int argc, const char * argv[]) {
     
-    //A while loop seems more appropriate here
-    //because I am more concerned with getting to 10
-    //than I am with the number times it loops.
+    //A for loop seemed more appropriate here
+    //because I wrote a little less code
     
-    //making the loop and printing it out per loop through
-    int num = 0;
+    //making the for loop and printing it out per loop through
+    for ( int x = 0; x <= 10; x = x + 1 ){
+        std::cout << x;
+    }
     //variable
+    int num = 0;
+    //making the while loop and printing it out per loop through
     while (num < 10){
         num = num + 1;
         std::cout << num;
     }
-    
     
     //next part
     
     //creating variables
     int input1;
     int input2;
-    
     //creating prompts for the user to enter two numbers
     std::cout << "\nEnter any whole number.\n";
     std::cin >> input1;
     std::cout << "Enter any other whole number larger than your first answer.\n";
     std::cin >> input2;
-    
-    //creating an if statement to let people know they did it wrong if they did it wrong
-    if (input1 > input2) {
-        std::cout << "Second response was not larger than your first.\n";
+    //creating a while loop in the if statement
+    if (input1 < input2) {
+        while (input1 < input2){
+            input1 = input1 + 1;
+            std::cout << input1;
+        }
     }
-    
-    //creating the while loop
-    while (input1 < input2){
-        input1 = input1 + 1;
-        std::cout << input1;
+    else {
+        while (input1 > input2){
+            input1 = input1 - 1;
+            std::cout << input1;
+        }
     }
     
     //next part
     
+    num = 0;
     
     
     
