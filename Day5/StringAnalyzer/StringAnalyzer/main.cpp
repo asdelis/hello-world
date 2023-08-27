da936f8b5242ee1fb590ded6d14874599a02cf16
andrew delis <andrew.s.delis@gmail.com>
2023-08-27 16:43:21 -0600

    first attempts at the first four functions

--- Day5/StringAnalyzer/StringAnalyzer/main.cpp
+++ Day5/StringAnalyzer/StringAnalyzer/main.cpp
@@ -20,10 +20,47 @@ bool IsTerminator(char letter){
     return isTerm;
 }
 
+//Returns whether or not the character is ., ?, !, or ,.
+bool IsPunctuation(char letter){
+    int intFormOfLetter = letter;
+    bool isPunc;
+    if (intFormOfLetter == 33 || intFormOfLetter == 44 || intFormOfLetter == 46 || intFormOfLetter == 63 ){
+        isPunc = true;
+    }
+    else {
+        isPunc = false;
+    }
+    return isPunc;
+}
+
+//Returns whether or not the character is a vowel.
+bool IsVowel(char letter){
+    int intFormOfLetter = letter;
+    bool isVwl;
+    if (intFormOfLetter == 65 || intFormOfLetter == 97 || intFormOfLetter == 69 || intFormOfLetter == 101 || intFormOfLetter == 73 || intFormOfLetter == 105 || intFormOfLetter == 79 || intFormOfLetter == 111 || intFormOfLetter == 85 || intFormOfLetter == 117 || intFormOfLetter == 89 || intFormOfLetter == 121){
+        isVwl = true;
+    }
+    else {
+        isVwl = false;
+    }
+    return isVwl;
+}
+
+bool IsConsonant(char letter){
+    int intFormOfLetter = letter;
+    bool isCon;
+    if (IsPunctuation(intFormOfLetter) == false && IsVowel(intFormOfLetter) == false){
+        isCon = true;
+    }
+    else {
+        isCon = false;
+    }
+    return isCon;
+}
+
+
 
 /*
-bool IsPunctuation(char c)
-bool IsVowel(char c)
 bool IsConsonant(char c)
 int NumWords(string s)
 int NumSentences(string s)
@@ -40,12 +77,14 @@ int main(int argc, const char * argv[]) {
     std::cout << "Enter a string containing one or more sentences: \n";
     std::getline(std::cin, input);
     
-    
-    
-    char letter;
-    std::cin >> letter;
-    letter = IsTerminator(letter);
-    std::cout << letter << "\n";
+    //Test that my first function works
+    //It did worked and returned a 0 for false and 1 for true
+    /*
+    char letter = input.front();
+    bool isTerm;
+    isTerm = IsTerminator(letter);
+    std::cout << isTerm << "\n";
+     */
     
     return 0;
 }
