e598a975885e685ad3fc4871c573a93fe531dbb1
andrew delis <andrew.s.delis@gmail.com>
2023-09-01 08:50:33 -0600

    finish the poker assignment

--- Day8/Poker/Poker/PokerTest.cpp
+++ Day8/Poker/Poker/PokerTest.cpp
@@ -8,23 +8,65 @@
 #include "Poker.hpp"
 
 int main(int argc, const char * argv[]) {
+    //STATS
+    //create deck
+    std::vector<card> statsDeck = {};
+    statsDeck = createDeck(statsDeck);
+    assert(statsDeck.size()==52);
     
+    //create shuffle loop and record stats
+    int flushCount = 0;
+    int straightCount = 0;
+    int straightFlushCount = 0;
+    int royalFlushCount = 0;
+    int fullHouseCount = 0;
+    for (int i = 0; i < 11; i++) {
+        shuffleDeck(statsDeck);
+        std::vector<card> statsDeckHand = {};
+        statsDeckHand = createHand(statsDeck);
+        if (checkFlush(statsDeckHand)) {
+            flushCount += 1;
+        }
+        else if (checkStraight(statsDeckHand)) {
+            straightCount += 1;
+        }
+        else if (checkStraightFlush(statsDeckHand)) {
+            straightFlushCount += 1;
+        }
+        else if (checkRoyalFlush(statsDeckHand)) {
+            royalFlushCount += 1;
+        }
+        else if (checkFullHouse(statsDeckHand)) {
+            fullHouseCount += 1;
+        }
+    }
+    std::cout << "Flush Count; " << flushCount << "\n";
+    std::cout << "Straight Count; " << straightCount << "\n";
+    std::cout << "straightFlushCount; " << straightFlushCount << "\n";
+    std::cout << "Royal Flush Count; " << royalFlushCount << "\n";
+    std::cout << "Full House Count; " << fullHouseCount << "\n";
+    
+    
+    
+    
+    
+    
+    
+    //FUNCTION TESTS
     //tests whether buildDeck works
     std::vector<card> deck = {};
     deck = createDeck(deck);
     assert(deck.size()==52);
-    
     //tests whether shuffleDeck works
     shuffleDeck(deck);
-    
     //tests whether printDeck works
     //printCardsInDeck(deck);
-    
     //tests whether createHand works
     std::vector<card> hand = {};
     hand = createHand(deck);
     assert(hand.size() == 5);
-    
+    //printing to see what is in that hand
+    //printCardsInDeck(hand);
     //tests whether checkFlush, checkStraight,
     //checkStraightFlush, and checkRoyalFlush works
     card card1 = {"spade",10};
@@ -37,7 +79,6 @@ int main(int argc, const char * argv[]) {
     assert(checkStraight(testHand) == true);
     assert(checkStraightFlush(testHand) == true);
     assert(checkRoyalFlush(testHand) == true);
-    
     //tests whether checkFullHouse works
     //by making two hands that should return true
     card card6 = {"spade",10};
@@ -55,9 +96,6 @@ int main(int argc, const char * argv[]) {
     std::vector<card> testHandThree = {card2, card3, card1, card4, card5};
     assert(checkFullHouse(testHandThree) == true);
     
-    
-    //printing to see what is in that hand
-    //printCardsInDeck(hand);
     
     
     
