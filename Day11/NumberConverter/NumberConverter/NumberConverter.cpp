35728aa08b849442c056d7c6f0ae5ba92260032c
andrew delis <andrew.s.delis@gmail.com>
2023-09-07 16:39:51 -0600

    finished the first part of part 2

--- Day11/NumberConverter/NumberConverter/NumberConverter.cpp
+++ Day11/NumberConverter/NumberConverter/NumberConverter.cpp
@@ -11,30 +11,29 @@
 //converts the string of numbers into integers with their real value (not ascii)
 //Uses a loop and the base ^ exponent * placeHolder formula to calculate the value
 int stringToInt(std::string num, int base) {
-    int  numValue = 0;
+    int numValue = 0;
     //first see if the number is negative
-    if (std::stoi(num) < 0) {
-        //if so enter this first loop
+    //binary and hexadecimal numbers can't be made negative with this sign: '-'
+    //so we only need to check this for decimal numbers entered into our string
+    if (num[0] == '-') {
         for (int i = 1; i < num.length(); i++) {
-            //an if to determine whether the character is a letter
-            if (tolower(num[i]) >= 'a' && tolower(num[i]) <= 'z'){
-                //converts their ascii value to their real value (formula given)
-                int realValue = tolower(num[i]) - 'a' + 10;
-                //formula to find the decimal value of the number entered
-                numValue -= (pow(base, (num.length() - 1 - i))) * realValue;
-            }
-            //same as above except this is for the characters that are numbers
-            //since their ascii conversion needs to follow a different formula
-            else {
-                int realValue = num[i] - '0';
-                numValue -= (pow(base, (num.length() - 1 - i))) * realValue;
-            }
+            //converts their ascii value to their real value (formula given)
+            int realValue = (num[i] - '0') * (-1);
+            //formula to find the decimal value of the number entered
+            numValue += (pow(base, (num.length() - 1 - i))) * realValue;
         }
     }
+    //same process for positive numbers
     else {
+        //except there is a need to determine whether the character is a letter
+        //so there is an if statement in the loop for positive numbers
         for (int i = 0; i < num.length(); i++) {
             if (tolower(num[i]) >= 'a' && tolower(num[i]) <= 'z'){
+                //converts their ascii value to their real value
+                //(different formula for non-numerical characters given too)
                 int realValue = tolower(num[i]) - 'a' + 10;
+                //formula changes a little bit too
+                //since the numbers are now positive (signs, realValue equation)
                 numValue += (pow(base, (num.length() - 1 - i))) * realValue;
             }
             else {
@@ -49,14 +48,16 @@ int stringToInt(std::string num, int base) {
 //PART 2
 std::string intToBinaryString(int numTwo){
     std::string decimalString = "";
-    int decimal = numTwo;
-    while (decimal > 1) {
-        int remainder = decimal % 2;
+    int updatedNum = numTwo;
+    while (updatedNum > 0) {
+        int remainder = updatedNum % 10;
         char remainderChar = remainder + '0';
         decimalString += remainderChar;
-        decimal = decimal / 2;
+        updatedNum /= 10;
     }
-    return decimalString;
+    std::string realDecimalString;
+    for (int i = decimalString.length(); i >= 0 ; i--) {
+        realDecimalString += decimalString[i];
+    }
+    return realDecimalString;
 }
-
-
--- Day11/NumberConverter/NumberConverterTest.cpp
+++ Day11/NumberConverter/NumberConverterTest.cpp
@@ -9,25 +9,24 @@
 
 int main(int argc, const char * argv[]) {
     
-    //DOESN'T WORK FOR NEGATIVE HEX NUMBERS RN
-    //PART 1
-    //read in the inputs for the stringToInt function
-    std::string num;
-    int base;
-    std::cout << "Please enter a number in its decimal, hexadecimal, or binary form: \n";
-    std::cin >> num;
-    std::cout << "Please enter the base of the number entered: \n";
-    std::cin >> base;
-    //use stringToInt formula to convert num to an integer
-    int output;
-    output = stringToInt(num, base);
-    std::cout << output << "\n";
-
+//    //DOESN'T WORK FOR NEGATIVE HEX NUMBERS RN
+//    //PART 1
+//    //read in the inputs for the stringToInt function
+//    std::string num;
+//    int base;
+//    std::cout << "Please enter a number in its decimal, hexadecimal, or binary form: \n";
+//    std::cin >> num;
+//    std::cout << "Please enter the base of the number entered: \n";
+//    std::cin >> base;
+//    //use stringToInt formula to convert num to an integer
+//    int output;
+//    output = stringToInt(num, base);
+//    std::cout << output << "\n";
     
     
     
     //PART 2
-    int numTwo;
+    int numTwo = 9345;
     std::cout << "Please enter another number in its decimal form: \n";
     std::cin >> numTwo;
     //test intToDecimalString
