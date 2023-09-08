06f303a5b9b5d0e0f83a21af24d126d19bd29458
andrew delis <andrew.s.delis@gmail.com>
2023-09-07 18:44:10 -0600

    finished the next question

--- Day11/NumberConverter/NumberConverter/NumberConverter.cpp
+++ Day11/NumberConverter/NumberConverter/NumberConverter.cpp
@@ -10,35 +10,35 @@
 //PART 1
 //converts the string of numbers into integers with their real value (not ascii)
 //Uses a loop and the base ^ exponent * placeHolder formula to calculate the value
-int stringToInt(std::string num, int base) {
+int stringToInt(std::string input, int base) {
     int numValue = 0;
     //first see if the number is negative
     //binary and hexadecimal numbers can't be made negative with this sign: '-'
     //so we only need to check this for decimal numbers entered into our string
-    if (num[0] == '-') {
-        for (int i = 1; i < num.length(); i++) {
+    if (input[0] == '-') {
+        for (int i = 1; i < input.length(); i++) {
             //converts their ascii value to their real value (formula given)
-            int realValue = (num[i] - '0') * (-1);
+            int realValue = (input[i] - '0') * (-1);
             //formula to find the decimal value of the number entered
-            numValue += (pow(base, (num.length() - 1 - i))) * realValue;
+            numValue += (pow(base, (input.length() - 1 - i))) * realValue;
         }
     }
     //same process for positive numbers
     else {
         //except there is a need to determine whether the character is a letter
         //so there is an if statement in the loop for positive numbers
-        for (int i = 0; i < num.length(); i++) {
-            if (tolower(num[i]) >= 'a' && tolower(num[i]) <= 'z'){
+        for (int i = 0; i < input.length(); i++) {
+            if (tolower(input[i]) >= 'a' && tolower(input[i]) <= 'z'){
                 //converts their ascii value to their real value
                 //(different formula for non-numerical characters given too)
-                int realValue = tolower(num[i]) - 'a' + 10;
+                int realValue = tolower(input[i]) - 'a' + 10;
                 //formula changes a little bit too
                 //since the numbers are now positive (signs, realValue equation)
-                numValue += (pow(base, (num.length() - 1 - i))) * realValue;
+                numValue += (pow(base, (input.length() - 1 - i))) * realValue;
             }
             else {
-                int realValue = num[i] - '0';
-                numValue += (pow(base, (num.length() - 1 - i))) * realValue;
+                int realValue = input[i] - '0';
+                numValue += (pow(base, (input.length() - 1 - i))) * realValue;
             }
         }
     }
@@ -46,18 +46,44 @@ int stringToInt(std::string num, int base) {
 }
 
 //PART 2
-std::string intToBinaryString(int numTwo){
-    std::string decimalString = "";
-    int updatedNum = numTwo;
-    while (updatedNum > 0) {
-        int remainder = updatedNum % 10;
+std::string intToDecimalString(int decimalIntInput){
+    //create a string
+    std::string initialString = "";
+    //initalize piece as the full amount, this will update everytime the loop ends
+    int piece = decimalIntInput;
+    while (piece > 0) {
+        //calculate the remainder
+        //which will be the value in the lowest place of the input integer numTwo
+        int remainder = piece % 10;
+        //make remainder a character
         char remainderChar = remainder + '0';
-        decimalString += remainderChar;
-        updatedNum /= 10;
+        //add that character to the string
+        initialString += remainderChar;
+        //update piece
+        piece /= 10;
     }
-    std::string realDecimalString;
-    for (int i = decimalString.length(); i >= 0 ; i--) {
-        realDecimalString += decimalString[i];
+    //create a new vector that we can use to reverse the order of decimalString
+    std::string outputString;
+    //loop through the string and reverse its order
+    for (int i = initialString.length(); i >= 0 ; i--) {
+        outputString += initialString[i];
     }
-    return realDecimalString;
+    //return the new string
+    return outputString;
+}
+
+std::string intToBinaryString(int binaryIntInput){
+    std::string initialString = "";
+    int piece = binaryIntInput;
+    while (piece > 0) {
+        int remainder = piece % 2;
+        char remainderChar = remainder + '0';
+        initialString += remainderChar;
+        piece /= 2;
+    }
+    std::string outputString;
+    for (int i = initialString.length(); i >= 0 ; i--) {
+        outputString += initialString[i];
+    }
+    return outputString;
 }
--- Day11/NumberConverter/NumberConverterTest.cpp
+++ Day11/NumberConverter/NumberConverterTest.cpp
@@ -12,27 +12,46 @@ int main(int argc, const char * argv[]) {
 //    //DOESN'T WORK FOR NEGATIVE HEX NUMBERS RN
 //    //PART 1
 //    //read in the inputs for the stringToInt function
-//    std::string num;
+//    std::string input;
 //    int base;
 //    std::cout << "Please enter a number in its decimal, hexadecimal, or binary form: \n";
-//    std::cin >> num;
+//    std::cin >> input;
 //    std::cout << "Please enter the base of the number entered: \n";
 //    std::cin >> base;
 //    //use stringToInt formula to convert num to an integer
 //    int output;
-//    output = stringToInt(num, base);
+//    output = stringToInt(input, base);
 //    std::cout << output << "\n";
+//
+//
+//
+//    //PART 2
+//    int decimalIntInput;
+//    std::cout << "Please enter another number in its decimal form: \n";
+//    std::cin >> decimalIntInput;
+//    //test intToDecimalString
+//    std::string decimalString;
+//    decimalString = intToDecimalString(decimalIntInput);
+//    std::cout << decimalString << "\n";
+//
+//    int binaryIntInput = 12;
+//    std::cout << "Please enter another number in its decimal form: \n";
+//    std::cin >> binaryIntInput;
+//    //test intToDecimalString
+//    std::string binaryString;
+//    binaryString = intToBinaryString(binaryIntInput);
+//    std::cout << binaryString << "\n";
     
-    
-    
-    //PART 2
-    int numTwo = 9345;
+    int hexIntInput = 12;
     std::cout << "Please enter another number in its decimal form: \n";
-    std::cin >> numTwo;
+    std::cin >> hexIntInput;
     //test intToDecimalString
-    std::string decimalString;
-    decimalString = intToBinaryString(numTwo);
-    std::cout << decimalString << "\n";
+    std::string hexString;
+    hexString = intToBinaryString(hexIntInput);
+    std::cout << hexString << "\n";
+    
+    
+    
     
     
     
